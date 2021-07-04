57cbda4defc61c867cd42ee57a2ae3436248956e
elx3020 <estebannl28@gmail.com>
2021-07-04 20:22:47 +0200

    custom web player update

--- custom-video-player/app.js
+++ custom-video-player/app.js
@@ -8,8 +8,9 @@ const panel = player.querySelector('.player_options');
 const toggle = player.querySelector('.play_button');
 const ranges = player.querySelectorAll('.player_slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
+const volumen = document.querySelector('.volume');
 
-// Builf out functions
+// Built out functions
 
 function togglePlay(){
     const method = video.paused ? 'play' : 'pause';
@@ -17,7 +18,7 @@ function togglePlay(){
 }
 
 function updateButton(){
-    const icon = this.paused ? '►' : '❚ ❚';
+    const icon = this.paused ? ' ►' : '❚❚';
     toggle.textContent = icon;
 
 
@@ -31,12 +32,13 @@ function skip(){
 
 function handleRangeUpdate(){
 
-    
     video[this.name] = this.value;
+    
+    
 }
 
-function handleKeyboad(){
-    console.log(this);
+function handleKeyboad(e){
+    console.log(e);
 }
 
 
@@ -52,26 +54,32 @@ function scrub(e){
     video.currentTime = scrubTime;
 }
 
+function activateSlider(){   
+    ranges[0].style.width = '150px';
+    ranges[0].style.opacity = 100;
+}
 
-
-function playerAnimations(){
-    
+function deactivateSlider(){
+    ranges[0].style.width = '0px';
+    ranges[0].style.opacity = 0;
 }
 
 
-
-// animations
-
-
-
-player.addEventListener('mouseenter',playerAnimations);
+function mute(){
+    video.muted = !video.muted;
+    if(video.muted) {
+        video[this.className] = 0;
+        ranges[0].value = video[this.className];
+    }else{
+        video[this.className] = 1;
+        ranges[0].value = video[this.className];
+    }
+}
 
 
 
 
 // control buttons
-
-
 
 video.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
@@ -86,11 +94,15 @@ ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
 
 ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
 
-
 let mouseDown = false;
 progress.addEventListener('click',scrub);
 progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
 progress.addEventListener('mousedown',() => mouseDown = true);
 progress.addEventListener('mouseup',() => mouseDown = false);
 
-// window.addEventListener('keydown',handleKeyboad);
+volumen.addEventListener('mouseenter',activateSlider);
+volumen.addEventListener('mouseleave',deactivateSlider);
+volumen.getElementsByTagName('i')[0].addEventListener('click',mute);
+
+
+window.addEventListener('keydown',handleKeyboad);
